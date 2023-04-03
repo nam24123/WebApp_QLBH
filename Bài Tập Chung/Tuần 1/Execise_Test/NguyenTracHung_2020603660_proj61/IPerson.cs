0d51ac66baa3d489e9442662acc9591ff899993f
Hùng Nguyễn <hungqaz38@gmail.com>
2023-04-03 19:37:10 +0700

    commit

---
+++ Bài Tập Chung/Tuần 1/Execise_Test/NguyenTracHung_2020603660_proj61/IPerson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NguyenTracHung_2020603660_proj61
+{
+    interface IPerson
+    {
+        string Skill();
+        DateTime DateOfBirth();
+        int Age();
+    }
+}
---
+++ Bài Tập Chung/Tuần 1/Execise_Test/NguyenTracHung_2020603660_proj61/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+namespace NguyenTracHung_2020603660_proj61
+{
+    class Program 
+    {
+        class DataToShortExp : Exception  
+        {
+            public DataToShortExp() : base("Dữ liệu quá ngắn") { }
+        }
+        class Employee 
+        {
+            int _id;
+            public int Id {
+                get
+                {
+                    return _id;
+                }
+                set
+                {
+                    _id = value;
+                }
+
+            }
+            string _name;
+            public string Name { get => _name;  
+                set {
+                    try
+                    {
+                        if (Name.Length < 3)
+                        {
+                            throw new Exception();
+                        }
+                        else
+                        {
+                            _name = Name;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        throw new DataToShortExp();
+                    }
+                }
+            }
+            Employee(int id)
+            {
+
+                _id = id;
+                _name = "No Name";
+            }
+
+            public Employee(int id, string name) : this(id)
+            {
+                try
+                {
+                    if (name.Length < 3)
+                    {
+                        throw new Exception();
+                    }
+                    else
+                    {
+                        _name = name;
+                    }
+                }
+                catch (Exception)
+                {
+                    throw new DataToShortExp();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(111,"1111");
+        }
+    }
+}
